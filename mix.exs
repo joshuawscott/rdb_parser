a9c0deca8c5f522bbd7acae2d0f8f95790f61b14
Joshua Scott <joshua.scott@gmail.com>
2018-03-10 18:56:20 -0600

    version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RdbParser.MixProject do
   def project do
     [
       app: :rdb_parser,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps()
